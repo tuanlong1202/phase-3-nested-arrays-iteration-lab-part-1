b6cdb474c8d227a5f244ddee1201c0ac5e9b2a14
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-25 11:39:15 -0400

    Phase 3 nested arrays iteration lan part 1

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,5 +2,16 @@ def find_even_values(src)
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
   # Output all even values in each nested array
+  rowIndex = 0
+  while rowIndex < src.count do
+    columnIndex = 0
+    while columnIndex < src[rowIndex].count do
+      if src[rowIndex][columnIndex] % 2 == 0
+        puts src[rowIndex][columnIndex]
+      end
+      columnIndex += 1
+    end
+    rowIndex += 1
+  end
 
 end
